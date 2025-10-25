11d783c54a00c2b4a6afb96309a4c44177e4e029
wanggao <wgjiachen@163.com>
2025-10-25 17:54:34 +0800

    model ts 异常修复

--- kb-task/src/code/Models/index.ts
+++ kb-task/src/code/Models/index.ts
@@ -2,16 +2,28 @@
  * 数据模型统一导出
  */
 
-export { User, type UserType } from 'code/Models/User'
-export { Project, type ProjectType } from 'code/Models/Project'
-export { ProjectMember, type ProjectMemberType } from 'code/Models/ProjectMember'
-export { Module, type ModuleType } from 'code/Models/Module'
-export { Task, type TaskType } from 'code/Models/Task'
-export { Tag, type TagType } from 'code/Models/Tag'
-export { TaskTag, type TaskTagType } from 'code/Models/TaskTag'
-export { TaskHistory, type TaskHistoryType } from 'code/Models/TaskHistory'
-export { TaskComment, type TaskCommentType } from 'code/Models/TaskComment'
-export { Notification, type NotificationType } from 'code/Models/Notification'
+import { User, type UserType } from 'code/Models/User'
+import { Project, type ProjectType } from 'code/Models/Project'
+import { ProjectMember, type ProjectMemberType } from 'code/Models/ProjectMember'
+import { Module, type ModuleType } from 'code/Models/Module'
+import { Task, type TaskType } from 'code/Models/Task'
+import { Tag, type TagType } from 'code/Models/Tag'
+import { TaskTag, type TaskTagType } from 'code/Models/TaskTag'
+import { TaskHistory, type TaskHistoryType } from 'code/Models/TaskHistory'
+import { TaskComment, type TaskCommentType } from 'code/Models/TaskComment'
+import { Notification, type NotificationType } from 'code/Models/Notification'
+
+// 重新导出所有模型和类型
+export { User, type UserType }
+export { Project, type ProjectType }
+export { ProjectMember, type ProjectMemberType }
+export { Module, type ModuleType }
+export { Task, type TaskType }
+export { Tag, type TagType }
+export { TaskTag, type TaskTagType }
+export { TaskHistory, type TaskHistoryType }
+export { TaskComment, type TaskCommentType }
+export { Notification, type NotificationType }
 
 // 导出所有模型实例供批量操作使用
 export const Models = {
